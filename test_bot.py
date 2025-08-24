de9214998f536a05fbc2e5609b7889db4ad72b6c
GStrade <80052709+GStrade@users.noreply.github.com>
2025-08-24 18:49:34 +0300

    Delete test_bot.py

--- test_bot.py
+++
@@ -1,9 +0,0 @@
-import os
-from telegram import Bot
-
-TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
-bot = Bot(token=TOKEN)
-bot.send_message(chat_id=CHAT_ID, text="🔔 בדיקה: זהו מסר ניסיון מהבוט.")
-print("נשלחה הודעה נסיונית")
